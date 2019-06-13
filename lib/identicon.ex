85c12f7ea37e4418cd1bc0da82e63d230891abad
Victor Lellis <vmlellis@gmail.com>
2019-06-12 23:42:22 -0300

    [FEAT] Identicon.draw_image/1

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,10 +6,20 @@ defmodule Identicon do
     |> build_grid()
     |> filter_odd_squares()
     |> build_pixel_map()
+    |> draw_image()
   end
 
-  defp build_pixel_map(image) do
-    %Identicon.Image{grid: grid} = image
+  defp draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  defp build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map = Enum.map grid, fn({_code, index}) ->
       horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
